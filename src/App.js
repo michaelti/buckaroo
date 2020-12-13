f91d635f7f7ee884d6543f75548b8cb502f8f321
Michael <m@michaelti.ca>
2020-12-13 10:51:36 -0500

    Set up sass

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Dashboard from "./pages/Dashboard";
+import "./App.scss";
 
 function App() {
     return (
-        <BrowserRouter>
-            <Navigation />
-            <Switch>
-                <Route path="/" exact>
-                    <Dashboard />
-                </Route>
-            </Switch>
-        </BrowserRouter>
+        <div className="app">
+            <BrowserRouter>
+                <Navigation />
+                <Switch>
+                    <Route path="/" exact>
+                        <Dashboard />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </div>
     );
 }
 

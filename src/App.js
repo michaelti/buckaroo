fcaf839be8da56ecc829ec73caaa42c19ea31b3b
Michael <m@michaelti.ca>
2020-12-13 10:42:31 -0500

    Add Navigation and Dashboard components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
     return (
         <BrowserRouter>
+            <Navigation />
             <Switch>
-                <Route path="/" exact></Route>
+                <Route path="/" exact>
+                    <Dashboard />
+                </Route>
             </Switch>
         </BrowserRouter>
     );

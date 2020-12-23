3fc4ce1a39d303486a03f6583a73345e14d24337
Michael <m@michaelti.ca>
2020-12-22 21:57:01 -0500

    Add grid and dashboard css

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,14 @@
+import "./Dashboard.scss";
+
 function Dashboard() {
-    return <div>Dashboard</div>;
+    return (
+        <main class="main">
+            <h1 className="main__title">👋 Welcome back, User</h1>
+            <span>
+                Here is your <strong>TD Chequing</strong> account at a glance
+            </span>
+        </main>
+    );
 }
 
 export default Dashboard;

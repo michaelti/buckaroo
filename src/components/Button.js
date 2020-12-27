737630d2187f01cd3687ee57a893da599ed7d6a6
Michael <m@michaelti.ca>
2020-12-27 18:29:25 -0500

    Fix warning for button links

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,19 +2,15 @@ import "./Button.scss";
 import { Link } from "react-router-dom";
 
 function Button({ isLink, children, ...props }) {
-    const normalBtn = (
+    return isLink ? (
+        <Link className="button" {...props}>
+            {children}
+        </Link>
+    ) : (
         <button className="button" {...props}>
             {children}
         </button>
     );
-
-    const linkBtn = (
-        <Link className="button" {...props}>
-            {children}
-        </Link>
-    );
-
-    return isLink ? linkBtn : normalBtn;
 }
 
 export default Button;
